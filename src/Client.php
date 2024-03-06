32284bd203531244c0db280c9d4e9cef3dbcd3f3
Art4 <art4@wlabs.de>
2024-03-06 08:58:43 +0100

    Implement /districts/<zip>.json endpoint

--- src/Client.php
+++ src/Client.php
@@ -58,6 +58,46 @@ final class Client
         return $data;
     }
 
+    /**
+     * Get one district by ZIP using the endpoint `GET /districts/<zip>.json`
+     *
+     * @link https://www.weg.li/api
+     *
+     * @throws \Psr\Http\Client\ClientExceptionInterface If an error happens while processing the request.
+     * @throws \Exception If an error happens while processing the response.
+     *
+     * @return mixed[]
+     */
+    public function getDistrictByZip(string $zip): array
+    {
+        $request = $this->requestFactory->createRequest('GET', 'https://www.weg.li/districts/' . $zip . '.json');
+        $request = $request->withHeader('Accept', 'application/json');
+
+        $response = $this->httpClient->sendRequest($request);
+
+        if ($response->getStatusCode() !== 200) {
+            throw new Exception('Server replied with status code ' . $response->getStatusCode());
+        }
+
+        if (! str_starts_with($response->getHeaderLine('content-type'), 'application/json')) {
+            throw new Exception('Server replied not with JSON content.');
+        }
+
+        $responseBody = $response->getBody()->__toString();
+
+        try {
+            $data = json_decode($responseBody, true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $th) {
+            throw new Exception('Response body contains no valid JSON: ' . $responseBody, 0, $th);
+        }
+
+        if (! is_array($data)) {
+            throw new Exception('Response JSON does not contain an array: ' . $responseBody);
+        }
+
+        return $data;
+    }
+
     private function __construct(
         private ClientInterface $httpClient,
         private RequestFactoryInterface $requestFactory,
